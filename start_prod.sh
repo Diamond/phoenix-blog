d2d9acb31b7dd92fdcb2cc229e55da8083dfb0ee
Brandon Richey <brichey@teladoc.com>
2015-07-04 00:46:15 -0400

    Modifying the start script slightly

--- start_prod.sh
+++ start_prod.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+npm install
+./node_modules/brunch/bin/brunch build --production
+mix phoenix.digest
 rm -rf ./config/prod.secret.exs
 ln -sf /config/pxblog/prod.secret.exs ./config/prod.secret.exs
 MIX_ENV=prod PORT=4001 elixir --detached -S mix phoenix.server
