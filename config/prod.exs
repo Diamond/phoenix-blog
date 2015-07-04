8bfc62d0ffbdc260ecb1d9c53b9bad9046bbcd95
Brandon Richey <brichey@teladoc.com>
2015-07-04 00:22:40 -0400

    First little bug (ish): start_prod should be in the root dir

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :pxblog, Pxblog.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com"],
+  url: [host: "brandonrichey.com"],
   cache_static_manifest: "priv/static/manifest.json"
 
 # ## SSL Support
